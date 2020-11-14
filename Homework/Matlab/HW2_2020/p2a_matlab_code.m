373c33bba8590aafd6b2724e28a37fcb75bafff7
Matthew Orth <mmorth98@gmail.com>
2020-11-14 08:07:30 -0600

    HW2_2020 P2a Matlab Complete.

--- Homework/Matlab/HW2_2020/p2a_matlab_code.m
+++ Homework/Matlab/HW2_2020/p2a_matlab_code.m
@@ -14,18 +14,21 @@ wpl = imread('C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework2\\HW2_2020\\HW2
 % Drawn the pawns on board 1
 figure, imshow(src1);
 matchBlackPawnFeaturesAndDraw(src1, bpd);
+matchBlackPawnFeaturesAndDraw(src1, bpl);
 matchWhitePawnFeaturesAndDraw(src1, wpd);
 matchWhitePawnFeaturesAndDraw(src1, wpl);
 
 % Drawn the pawns on board 2
 figure, imshow(src2);
 matchBlackPawnFeaturesAndDraw(src2, bpd);
+matchBlackPawnFeaturesAndDraw(src2, bpl);
 matchWhitePawnFeaturesAndDraw(src2, wpd);
 matchWhitePawnFeaturesAndDraw(src2, wpl);
 
 % Drawn the pawns on board 3
 figure, imshow(src3);
 matchBlackPawnFeaturesAndDraw(src3, bpd);
+matchBlackPawnFeaturesAndDraw(src3, bpl);
 matchWhitePawnFeaturesAndDraw(src3, wpd);
 matchWhitePawnFeaturesAndDraw(src3, wpl);
 
@@ -39,13 +42,26 @@ function matchBlackPawnFeaturesAndDraw(src, temp)
     indexPairs = matchFeatures(features1, features2);
 
     matchedPoints1 = valid_points1(indexPairs(:,1),:);
-    matchedPoints2 = valid_points2(indexPairs(:,2),:);
-
-    hold on;
-    for i = 1:2:size(matchedPoints1,1)
-        rectangle('Position', [matchedPoints1.Location(i,1)-22, matchedPoints1.Location(i,2)-32, 59, 59], 'EdgeColor', 'r', 'LineWidth', 3);
+    
+    colMatch = 0;
+    lastX = 0;
+    i = 1;
+    while i <= size(matchedPoints1,1)
+        if lastX == 0 || colMatch == 0
+            rectangle('Position', [60*floor((matchedPoints1.Location(i,1)-21)/60)+1, 60*floor((matchedPoints1.Location(i,2)-25)/60), 58, 58], 'EdgeColor', 'r', 'LineWidth', 3);
+            lastX = matchedPoints1.Location(i,1);
+            colMatch = colMatch + 1;
+            i = i + 1;
+        elseif abs(matchedPoints1.Location(i,1) - lastX) < 1
+            rectangle('Position', [60*floor((matchedPoints1.Location(i,1)-21)/60)+1, 60*floor((matchedPoints1.Location(i,2)-25)/60), 58, 58], 'EdgeColor', 'r', 'LineWidth', 3);
+            lastX = matchedPoints1.Location(i,1);
+            colMatch = colMatch + 1;
+            i = i + 1;
+        else
+            i = i + 2*colMatch - colMatch;
+            colMatch = 0;
+        end
     end
-    figure; showMatchedFeatures(src,temp,matchedPoints1,matchedPoints2);
 end
 
 function matchWhitePawnFeaturesAndDraw(src, temp)
@@ -58,10 +74,24 @@ function matchWhitePawnFeaturesAndDraw(src, temp)
     indexPairs = matchFeatures(features1, features2);
 
     matchedPoints1 = valid_points1(indexPairs(:,1),:);
-    matchedPoints2 = valid_points2(indexPairs(:,2),:);
-
-    for i = 1:4:size(matchedPoints1,1)
-        rectangle('Position', [matchedPoints1.Location(i,1)-21, matchedPoints1.Location(i,2)-25, 59, 59], 'EdgeColor', 'r', 'LineWidth', 3);
+    
+    colMatch = 0;
+    lastX = 0;
+    i = 1;
+    while i <= size(matchedPoints1,1)
+        if lastX == 0 || colMatch == 0
+            rectangle('Position', [60*floor((matchedPoints1.Location(i,1)-21)/60)+1, 60*floor((matchedPoints1.Location(i,2)-25)/60), 58, 58], 'EdgeColor', 'r', 'LineWidth', 3);
+            lastX = matchedPoints1.Location(i,1);
+            colMatch = colMatch + 1;
+            i = i + 1;
+        elseif abs(matchedPoints1.Location(i,1) - lastX) < 1
+            rectangle('Position', [60*floor((matchedPoints1.Location(i,1)-21)/60)+1, 60*floor((matchedPoints1.Location(i,2)-25)/60), 58, 58], 'EdgeColor', 'r', 'LineWidth', 3);
+            lastX = matchedPoints1.Location(i,1);
+            colMatch = colMatch + 1;
+            i = i + 1;
+        else
+            i = i + 4*colMatch - colMatch;
+            colMatch = 0;
+        end
     end
-    figure; showMatchedFeatures(src,temp,matchedPoints1,matchedPoints2);
 end
