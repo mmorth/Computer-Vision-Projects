5376c4f94e51eb511cd9027f03eb37f7365c5e38
Matthew Orth <mmorth98@gmail.com>
2020-11-18 07:49:31 -0600

    H3_2020 P3a Complete.

--- Homework/Qt/OpenCV/HW3_2018/p3a_opencv_code.cpp
+++ Homework/Qt/OpenCV/HW3_2018/p3a_opencv_code.cpp
@@ -8,29 +8,24 @@
 //using namespace std;
 //using namespace cv;
 
+//bool firstPoint = true;
+//Point lastPoint;
+//void markPacmanPath(Mat frame, Mat mask, Mat path);
+//Mat createGridMask(Mat frame);
 //void processVideo(VideoCapture cap, string inputVideoName, string outputVideoName);
 
 //int main()
 //{
 //    char keyPress;
-//    cout << "p1b_opencv_code\n";
-
-//    Mat frame = imread("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2018\\HW3\\input\\part1a\\frame.png");
-//    imshow("frame", frame);
-
-//    Mat yellowMask;
-//    inRange(frame, Scalar(0, 200, 200), Scalar(200, 255, 255), yellowMask);
-//    frame.setTo(Scalar(0, 255, 0), yellowMask);
-
-//    // TODO: Need to find the bounding box of Pacman and only filter the colors in the bounding box region
-
-//    imshow("after", frame);
+//    cout << "p3a_opencv_code\n";
 
 //    // Read input videos
-////    VideoCapture cap1("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2018\\HW3\\input\\part1a\\input1a1.avi");
-////    processVideo(cap1, "input1a1.avi", "p1a_output1.avi");
-////    VideoCapture cap2("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2018\\HW3\\input\\part1a\\input1a2.avi");
-////    processVideo(cap2, "input1a2.avi", "p1a_output2.avi");
+//    VideoCapture cap1("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2018\\HW3\\input\\part3a\\input3a1.avi");
+//    processVideo(cap1, "input3a1.avi", "p3a_output1.avi");
+//    lastPoint = Point(1, 1);
+//    firstPoint = true;
+//    VideoCapture cap2("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2018\\HW3\\input\\part3a\\input3a2.avi");
+//    processVideo(cap2, "input3a2.avi", "p3a_output2.avi");
 
 //    while(1)
 //    {
@@ -60,6 +55,10 @@
 //    int frame_width = cap.get(CAP_PROP_FRAME_WIDTH);
 //    int frame_height = cap.get(CAP_PROP_FRAME_HEIGHT);
 
+//    // Create a Mat that will store Pacman's path
+//    Mat path(Size(frame_width, frame_height), 16);
+//    path = Scalar(0,0,0);
+
 //    // Create a video writer and process the input video frame by frame
 //    printf("Processing video %s...\n", inputVideoName.c_str());
 //    VideoWriter video(outputVideoName,VideoWriter::fourcc('M','J','P','G'),30, Size(frame_width,frame_height));
@@ -73,9 +72,9 @@
 //      if (frame.empty())
 //        break;
 
-//      Mat blueMask;
-//      inRange(frame, Scalar(65, 0, 0), Scalar(195, 70, 70), blueMask);
-//      frame.setTo(Scalar(0, 255, 0), blueMask);
+//      // Apply green color to Pacman in the frame
+//      Mat mask = createGridMask(frame);
+//      markPacmanPath(frame, mask, path);
 
 //      // Write the frame into the output video file
 //      video.write(frame);
@@ -86,6 +85,8 @@
 //        break;
 //    }
 
+//    imwrite("path.png", path);
+
 //    // Clean up video capture and writer
 //    cap.release();
 //    video.release();
@@ -94,4 +95,65 @@
 //    destroyAllWindows();
 
 //    printf("Video processing complete for %s!\n", outputVideoName.c_str());
+//}
+
+//// Create a mask for the board to prevent matching of the Pacman representing the player's lives
+//Mat createGridMask(Mat frame) {
+//    Mat mask(Size(224, 288), 16);
+//    mask = 0;
+//    Rect roi(0, 0, 224, 270);
+//    Mat temp = mask(roi);
+//    temp = Scalar(255, 255, 255);
+//    temp.copyTo(mask(roi));
+
+//    Mat result;
+//    bitwise_and(frame, mask, result);
+
+//    return result;
+//}
+
+//// Colors Pacman green
+//void markPacmanPath(Mat frame, Mat mask, Mat path) {
+//    // Create the mask to isolate the Pacman colors
+//    Mat yellowMask;
+//    inRange(mask, Scalar(0, 200, 200), Scalar(210, 255, 255), yellowMask);
+
+//    // Convert result to CV_8U to support finding contours
+//    Mat resb;
+//    yellowMask.convertTo(resb, CV_8U, 255);
+
+//    // Find the contours that could correspond to Pacman
+//    vector<vector<Point>> contours;
+//    findContours(resb, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+
+//    // Only draw the contours that are large enough (e.g. remove the noise of smaller objects with similar colors to Pacman)
+//    for (int i = 0; i < contours.size(); i++) {
+//        if (contourArea(contours[i]) > 100) {
+//            // Find Pacman's center position
+//            Rect br = boundingRect(contours[i]);
+//            Vec3b color;
+//            color.val[0] = 255;
+//            color.val[1] = 255;
+//            color.val[2] = 255;
+//            Point curPoint = Point(br.x+br.width/2, br.y+br.height/2);
+//            if (!firstPoint && abs(curPoint.x-lastPoint.x) < 100) {
+//                // Add the line from the last point to the current point to the path
+//                std::vector<cv::Point> contour;
+//                contour.push_back(lastPoint);
+//                contour.push_back(curPoint);
+//                std::vector<std::vector<cv::Point> > contourVec;
+//                contourVec.push_back(contour);
+
+//                cv::drawContours(path,contourVec,0,color);
+//                lastPoint = curPoint;
+//            } else {
+//                // Add the first point
+//                lastPoint = curPoint;
+//                firstPoint = false;
+//            }
+//        }
+//    }
+
+//    // Add the path to the frame
+//    frame = frame + path;
 //}
