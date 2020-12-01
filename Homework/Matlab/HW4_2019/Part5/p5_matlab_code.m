b132452d7ec32eb0122da8c8bb513a8b99b07938
Matthew Orth <mmorth98@gmail.com>
2020-12-01 17:00:22 -0600

    HW4_2019 P5 Complete.

--- Homework/Matlab/HW4_2019/Part5/p5_matlab_code.m
+++ Homework/Matlab/HW4_2019/Part5/p5_matlab_code.m
@@ -1,13 +1,18 @@
-drawSpectrogram('ball_bounce_brick_mono.wav');
-drawSpectrogram('ball_bounce_paddle_mono.wav');
-drawSpectrogram('paddle_widening_mono.wav');
+% Call function to draw spectrogram for all audio files
+drawSpectrogram('PokemonBattleMusic.wav');
+drawSpectrogram('PokemonHealingSounds.wav');
+drawSpectrogram('PokemonMusicWall.wav');
 
+% Function that draws the spectrogram of input audio file
 function drawSpectrogram(fileName)
+    % Read the audio
     [Y,fs]=audioread(fileName); % read the WAV file
 
+    % FFT and inverse FFT (can be changed to updated spectrogram format)
     Y = fft(Y);
     Y = ifft(Y);
 
+    % Display the resulting spectrogram
     figure; spectrogram(Y(:,1), 512, 256, 512, fs, 'yaxis'); 
     title(strcat(strcat('Spectrogram for =', fileName)));
 end
--- Homework/Matlab/HW4_2019/Part5/p5_record_audio_samples.m
+++ Homework/Matlab/HW4_2019/Part5/p5_record_audio_samples.m
@@ -1,5 +1,13 @@
-recObj = audiorecorder;
-disp('Start Speaking.');
+% Configure the Matlab audio recorder
+Fs = 44100;
+numBits = 24;
+numChannels = 1;
+
+% Record the game audio
+recObj = audiorecorder(Fs, numBits, numChannels);
+disp('Start Recording...');
 recordblocking(recObj,5);
 disp('End of Recording');
-play(recObj);
+
+% Save the resulting game audio to a file
+audiowrite('PokemonMusic.wav', getaudiodata(recObj), Fs);
