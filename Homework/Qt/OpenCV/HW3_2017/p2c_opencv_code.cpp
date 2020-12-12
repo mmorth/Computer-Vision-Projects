4bc9d2ea18151d7aa7ef229885e3da8f70d1e40c
Matthew Orth <mmorth98@gmail.com>
2020-12-12 09:45:08 -0600

    Traffic monitor complete.

--- Homework/Qt/OpenCV/HW3_2017/p2c_opencv_code.cpp
+++ Homework/Qt/OpenCV/HW3_2017/p2c_opencv_code.cpp
@@ -8,25 +8,36 @@
 //using namespace std;
 //using namespace cv;
 
-//int frameNum = 0;
+//struct vehicle {
+//    Rect br;
+//    Point centroid;
+//    int lastFrameSeen;
+//    int direction = -1;
+//};
+
+//vector<vehicle> objects;
 
 //void processVideo(VideoCapture cap, Mat firstFrame, string inputVideoName, string outputVideoName);
-//void detectBuses(Mat firstFrame, Mat frame, Mat mask);
-//void drawBusesOnFrame(Mat firstFrame, Mat videoFrame, Rect location);
+//void detectVehicles(Mat firstFrame, Mat videoFrame, Mat mask, int frameNum);
+//void drawCarsOnFrame(Mat firstFrame, Mat videoFrame, Rect location);
+//int checkExistingObjects(Rect br, int frameNum);
+//void removeOldObjects(int frameNum);
+
+//int numBuses = 0;
+//int numCars = 0;
+//int numPeople = 0;
 
 //int main()
 //{
-//    cout << "p2b_opencv_code\n";
+//    cout << "p2c_opencv_code\n";
 
 //    // Read input videos
 //    VideoCapture cap1("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2016\\1_Traffic_Monitoring\\clip1.avi");
 //    Mat video1Frame = imread("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2016\\1_Traffic_Monitoring\\video1Frame.png");
 //    processVideo(cap1, video1Frame, "clip1.avi", "output1.avi");
-
 //    VideoCapture cap2("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2016\\1_Traffic_Monitoring\\clip2.avi");
 //    Mat video2Frame = imread("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2016\\1_Traffic_Monitoring\\video2Frame.png");
 //    processVideo(cap2, video2Frame, "clip2.avi", "output2.avi");
-
 //    VideoCapture cap3("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2016\\1_Traffic_Monitoring\\clip3.avi");
 //    Mat video3Frame = imread("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\Homework3\\HW3_2016\\1_Traffic_Monitoring\\video3Frame.png");
 //    processVideo(cap3, video3Frame, "clip3.avi", "output3.avi");
@@ -54,6 +65,7 @@
 //    Mat fgMask;
 
 //    // Create a video writer and process the input video frame by frame
+//    int frameNum = 0;
 //    printf("Processing video %s...\n", inputVideoName.c_str());
 //    VideoWriter video(outputVideoName,VideoWriter::fourcc('M','J','P','G'),30, Size(frame_width,frame_height), true);
 //    while(1)
@@ -63,22 +75,24 @@
 //      cap >> frame;
 //      frameNum++;
 
-//      Mat backgroundFrame = firstFrame.clone();
-
 //      // If the frame is empty, break immediately
 //      if (frame.empty())
 //        break;
+
+//      Mat backgroundFrame = firstFrame.clone();
 
 //      // Run the background subtraction model
 //      pBackSub->apply(frame, fgMask);
 
 //      // Detect vechicles from the background subtraction mask
-//      detectBuses(backgroundFrame, frame, fgMask);
-//      imshow("backgroundFrame", backgroundFrame);
+//      detectVehicles(backgroundFrame, frame, fgMask, frameNum);
 //      imshow("frame", frame);
+//      imshow("backgroundFrame", backgroundFrame);
+
+////      imwrite("frame" + std::to_string(frameNum) + ".png", frame);
 
 //      // Write the frame into the output video file
-////      video.write(backgroundFrame);
+////      video.write(frame);
 
 //      // Press  ESC on keyboard to  exit
 //      char c = (char)waitKey(1);
@@ -97,7 +111,7 @@
 //}
 
 //// Detect vehicles in the frame and count total number
-//void detectBuses(Mat firstFrame, Mat videoFrame, Mat mask) {
+//void detectVehicles(Mat firstFrame, Mat frame, Mat mask, int frameNum) {
 //    // Threshold to pull out the main part of the object
 //    threshold(mask, mask, 200, 255, THRESH_BINARY);
 //    int width = 1;
@@ -117,33 +131,76 @@
 
 //    // Determine whether detected objects are a vehicle or not
 //    for (int i = 0; i < contours.size(); i++) {
-//        // Compute the bounding rectangle of the detected contour
+//        // Determine which type of object is detected
 //        Rect br = boundingRect(contours[i]);
 
-//        // Ensure that the detected contour is a bus
-//        if (contourArea(contours[i]) > 1000 && br.y > 50 && br.height > 60 && br.height < 90) {
-//            // Draw the bus on the frame
-//            drawBusesOnFrame(firstFrame, videoFrame, br);
+//        // Ensure that the detected contour has the possibility of being a vehicle or person
+//        if (contourArea(contours[i]) > 500 && br.y > 50 && br.height < 60 && br.height > 30) {
+//            // Check whether this object is below min distance threshold to classify as other object
+//            int vehicleNum = checkExistingObjects(br, frameNum);
+//            if (vehicleNum == -1) {
+//                // Add new detected vehicle to the frame
+//                vehicle v;
+//                v.br = br;
+//                v.centroid = Point(br.x+br.width/2, br.y+br.height/2);
+//                v.lastFrameSeen = frameNum;
+//                objects.push_back(v);
+//            } else {
+//                // If vehicle is moving from left to right, draw on the frame
+//                if (objects.at(vehicleNum).direction == 1) {
+//                    // Car is traveling from left to right so draw it on the frame
+//                    drawCarsOnFrame(firstFrame, frame, br);
+//                }
+//            }
+
+//            // Draw the bounding box around the vehicle
+//            rectangle(frame, br, Scalar(0, 0, 255), 1);
 //        }
+//    }
+
+//    // Remove old objects from the frame
+//    removeOldObjects(frameNum);
+//}
+
+//// Checks if the current object has already been identified in a prior frame
+//int checkExistingObjects(Rect br, int frameNum) {
+//    Point centroid = Point(br.x+br.width/2, br.y+br.height/2);
+//    for (int i = 0; i < objects.size(); i++) {
+//        // Check if distance between objects is less than threshold of 10 pixels
+//        double distance = cv::norm(centroid-objects.at(i).centroid);
+//        if (distance < 50) {
+//            // Object has already been detected in prior frame, update information
+//            if (objects.at(i).direction == -1) {
+//                if (centroid.x - objects.at(i).centroid.x > -5) {
+//                    objects.at(i).direction = 1;
+//                } else {
+//                    objects.at(i).direction = 0;
+//                }
+//            }
+
+//            objects.at(i).lastFrameSeen = frameNum;
+//            objects.at(i).br = br;
+//            objects.at(i).centroid = centroid;
+//            return i;
+//        }
+//    }
+
+//    // Object has not been counted in a previous frame
+//    return -1;
+//}
+
+//// Removes objects from the vector when they have not been seen for a while
+//void removeOldObjects(int frameNum) {
+//    for (vector<vehicle>::iterator it=objects.begin();it!=objects.end();) {
+//       if(abs(frameNum-it->lastFrameSeen) > 10) {
+//          it = objects.erase(it);
+//       } else
+//          ++it;
 //    }
 //}
 
-//// Draws the detected bus on the frame
-//void drawBusesOnFrame(Mat firstFrame, Mat videoFrame, Rect location) {
-//    // Filter for the red bus color
-//    Mat mask;
-//    inRange(videoFrame, Scalar(50, 55, 150), Scalar(80, 80, 200), mask);
-
-//    // Convert result to CV_8U to support finding contours
-//    Mat resb;
-//    mask.convertTo(resb, CV_8U, 255);
-
-//    // Find the contours of the masked image
-//    vector<vector<Point>> contours;
-//    findContours(resb, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-
-//    if (contours.size() > 0) {
-//        Mat bus = videoFrame(location);
-//        bus.copyTo(firstFrame(location));
-//    }
+//// Draws the detected car on the frame
+//void drawCarsOnFrame(Mat firstFrame, Mat videoFrame, Rect location) {
+//    Mat car = videoFrame(location);
+//    car.copyTo(firstFrame(location));
 //}
