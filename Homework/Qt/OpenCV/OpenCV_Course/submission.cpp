250cf76c5283bf4ee4e7e7ab1406ce08225003cf
Matthew Orth <mmorth98@gmail.com>
2021-05-12 07:57:17 -0500

    Updated files to be in submission form.

--- Homework/Qt/OpenCV/OpenCV_Course/submission.cpp
+++ Homework/Qt/OpenCV/OpenCV_Course/submission.cpp
@@ -1,53 +1,53 @@
-//#include <opencv2/core.hpp>
-//#include <opencv2/imgproc.hpp>
-//#include <opencv2/highgui.hpp>
-//#include <vector>
-//#include <iostream>
-//#include <math.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <vector>
+#include <iostream>
+#include <math.h>
 
-//using namespace cv;
-//using namespace std;
+using namespace cv;
+using namespace std;
 
-//// Points to store the center of the circle and a point on the circumference
-//Point topLeft, bottomRight;
-//// Source image
-//Mat source;
+// Points to store the center of the circle and a point on the circumference
+Point topLeft, bottomRight;
+// Source image
+Mat source;
 
-//// function which will be called on mouse input
-//void drawRectangleAndSaveImage(int action, int x, int y, int flags, void *userdata)
-//{
-//  // Action to be taken when left mouse button is pressed
-//  if( action == EVENT_LBUTTONDOWN )
-//  {
-//    topLeft = Point(x,y);
-//  }
-//  // Action to be taken when left mouse button is released
-//  else if( action == EVENT_LBUTTONUP)
-//  {
-//    bottomRight = Point(x,y);
-//    rectangle(source, topLeft, bottomRight, Scalar(255, 0, 255), 5, LINE_8);
-//    imwrite("face.png", source);
-//    imshow("Window", source);
-//  }
-//}
+// function which will be called on mouse input
+void drawRectangleAndSaveImage(int action, int x, int y, int flags, void *userdata)
+{
+  // Action to be taken when left mouse button is pressed
+  if( action == EVENT_LBUTTONDOWN )
+  {
+    topLeft = Point(x,y);
+  }
+  // Action to be taken when left mouse button is released
+  else if( action == EVENT_LBUTTONUP)
+  {
+    bottomRight = Point(x,y);
+    rectangle(source, topLeft, bottomRight, Scalar(255, 0, 255), 5, LINE_8);
+    imwrite("face.png", source);
+    imshow("Window", source);
+  }
+}
 
-//int main()
-//{
-//  source = imread("C:\\Users\\mmort\\GIT\\CprE575\\Homework\\OpenCVCourse\\week2-cpp\\sample.jpg");
-//  // Make a dummy image, will be useful to clear the drawing
-//  Mat dummy = source.clone();
-//  namedWindow("Window");
-//  // highgui function called when mouse events occur
-//  setMouseCallback("Window", drawRectangleAndSaveImage);
-//  int k=0;
-//  // loop until escape character is pressed
-//  while(k!=27)
-//  {
-//    imshow("Window", source );
-//    k= waitKey(20) & 0xFF;
-//    if(k== 99)
-//            // Another way of cloning
-//            dummy.copyTo(source);
-//  }
-//  return 0;
-//}
+int main()
+{
+  source = imread("sample.jpg");
+  // Make a dummy image, will be useful to clear the drawing
+  Mat dummy = source.clone();
+  namedWindow("Window");
+  // highgui function called when mouse events occur
+  setMouseCallback("Window", drawRectangleAndSaveImage);
+  int k=0;
+  // loop until escape character is pressed
+  while(k!=27)
+  {
+    imshow("Window", source );
+    k= waitKey(20) & 0xFF;
+    if(k== 99)
+            // Another way of cloning
+            dummy.copyTo(source);
+  }
+  return 0;
+}
---
+++ OpenCV_ComputerVisionI_Course/mmorth_Assignment3/submission.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+// Set the parameters for the trackbars
+int maxScaleUp = 100;
+int scaleFactor = 1;
+int scaleType = 0;
+int maxType = 1;
+Mat im;
+
+string windowName = "Resize Image";
+string trackbarValue = "Scale";
+string trackbarType = "Type: \n 0: Scale Up \n 1: Scale Down";
+
+// Create method stub for callback function
+void scaleImage(int, void*);
+
+int main(){
+    // load an image
+    im = imread("truth.png");
+
+    // Create a window to display results
+    namedWindow(windowName, WINDOW_AUTOSIZE);
+
+    // Create the trackbars for the scaling factor and type
+    createTrackbar(trackbarValue, windowName, &scaleFactor, maxScaleUp, scaleImage);
+    createTrackbar(trackbarType, windowName, &scaleType, maxType, scaleImage);
+
+    // Display the image with a default value
+    scaleImage(25,0);
+
+    while (true){
+        // Continue until the escape button is pressed
+        int c;
+        c = waitKey(20);
+        if (static_cast<char>(c) == 27)
+            break;
+    }
+
+    return 0;
+}
+
+// Callback function for scaling image
+void scaleImage(int, void*){
+    double scaleFactorDouble = 1.0;
+    if (scaleType == 0) {
+        // Scale up
+        scaleFactorDouble = 1 + scaleFactor/100.0;
+    } else {
+        // Scale down
+        scaleFactorDouble = 1 - scaleFactor/100.0;
+    }
+
+    // Ensure edge conditions are taken care of
+    if (scaleFactorDouble == 0){
+        scaleFactorDouble = 0.01;
+    }
+
+    Mat scaledImage;
+    resize(im, scaledImage, Size(), scaleFactorDouble, scaleFactorDouble, INTER_LINEAR);
+    imshow(windowName, scaledImage);
+}
+
