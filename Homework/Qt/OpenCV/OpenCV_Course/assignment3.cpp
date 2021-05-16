34b70ce5333db6df24270896a251ad801544aaee
Matthew Orth <mmorth98@gmail.com>
2021-05-16 08:13:46 -0500

    Final draft of Assignment 2, 3, and 4.

--- Homework/Qt/OpenCV/OpenCV_Course/assignment3.cpp
+++ Homework/Qt/OpenCV/OpenCV_Course/assignment3.cpp
@@ -6,63 +6,51 @@
 //using namespace std;
 //using namespace cv;
 
-//// Set the parameters for the trackbars
 //int maxScaleUp = 100;
 //int scaleFactor = 1;
 //int scaleType = 0;
 //int maxType = 1;
-//Mat im;
 
 //string windowName = "Resize Image";
 //string trackbarValue = "Scale";
 //string trackbarType = "Type: \n 0: Scale Up \n 1: Scale Down";
+//Mat im;
 
-//// Create method stub for callback function
 //void scaleImage(int, void*);
 
 //int main(){
-//    // load an image
-//    im = imread("truth.png");
+//// load an image
+//im = imread("../data/images/truth.png");
 
-//    // Create a window to display results
-//    namedWindow(windowName, WINDOW_AUTOSIZE);
+//// Create a window to display results
+//namedWindow(windowName, WINDOW_AUTOSIZE);
 
-//    // Create the trackbars for the scaling factor and type
-//    createTrackbar(trackbarValue, windowName, &scaleFactor, maxScaleUp, scaleImage);
-//    createTrackbar(trackbarType, windowName, &scaleType, maxType, scaleImage);
+//createTrackbar(trackbarValue, windowName, &scaleFactor, maxScaleUp, scaleImage);
+//createTrackbar(trackbarType, windowName, &scaleType, maxType, scaleImage);
 
-//    // Display the image with a default value
-//    scaleImage(25,0);
+//scaleImage(25,0);
 
-//    while (true){
-//        // Continue until the escape button is pressed
-//        int c;
-//        c = waitKey(20);
-//        if (static_cast<char>(c) == 27)
-//            break;
-//    }
-
-//    return 0;
+//while (true){
+//    int c;
+//    c = waitKey(20);
+//    if (static_cast<char>(c) == 27)
+//        break;
 //}
 
-//// Callback function for scaling image
+//return 0;
+//}
+
+//// Callback functions
 //void scaleImage(int, void*){
-//    double scaleFactorDouble = 1.0;
-//    if (scaleType == 0) {
-//        // Scale up
+//    double scaleFactorDouble;
+//    if (scaleType == 0){
 //        scaleFactorDouble = 1 + scaleFactor/100.0;
 //    } else {
-//        // Scale down
 //        scaleFactorDouble = 1 - scaleFactor/100.0;
 //    }
-
-//    // Ensure edge conditions are taken care of
-//    if (scaleFactorDouble == 0){
-//        scaleFactorDouble = 0.01;
-//    }
-
+//    if (scaleFactor == 0)
+//        scaleFactor = 1;
 //    Mat scaledImage;
 //    resize(im, scaledImage, Size(), scaleFactorDouble, scaleFactorDouble, INTER_LINEAR);
 //    imshow(windowName, scaledImage);
 //}
-
