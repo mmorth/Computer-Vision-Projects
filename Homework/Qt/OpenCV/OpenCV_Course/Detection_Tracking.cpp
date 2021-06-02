2715d41aaec9a3e98c9e4c9b694b86150005c20d
Matthew Orth <mmorth98@gmail.com>
2021-06-02 07:30:39 -0500

    Have ball detection framework.

--- Homework/Qt/OpenCV/OpenCV_Course/Detection_Tracking.cpp
+++ Homework/Qt/OpenCV/OpenCV_Course/Detection_Tracking.cpp
@@ -12,11 +12,20 @@
 
 using namespace std;
 using namespace cv;
+using namespace dnn;
 
 #define SSTR( x ) static_cast< std::ostringstream & >( ( std::ostringstream() << std::dec << x ) ).str()
 
+// Initialize the YOLO parameters
+float objectnessThreshold = 0.5; // Objectness threshold
+float confThreshold = 0.5; // Confidence threshold
+float nmsThreshold = 0.4;  // Non-maximum suppression threshold
+int inpWidth = 416;  // Width of network's input image
+int inpHeight = 416; // Height of network's input image
+vector<string> classes;
+
 // Get the names of the output layers
-auto getOutputsNames(const Net& net)
+vector<String> getOutputsNames(const Net& net)
 {
     static vector<String> names;
     if (names.empty())
@@ -32,12 +41,18 @@ auto getOutputsNames(const Net& net)
         for (size_t i = 0; i < outLayers.size(); ++i)
         names[i] = layersNames[outLayers[i] - 1];
     }
+
     return names;
 }
 
 // Draw the predicted bounding box
-void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
+bool drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
 {
+    if (classes[classId] != "sports ball")
+    {
+        return false;
+    }
+
     //Draw a rectangle displaying the bounding box
     rectangle(frame, Point(left, top), Point(right, bottom), Scalar(255, 178, 50), 3);
 
@@ -53,12 +68,16 @@ void drawPred(int classId, float conf, int left, int top, int right, int bottom,
     int baseLine;
     Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
     top = max(top, labelSize.height);
-    rectangle(frame, Point(left, top - round(1.5*labelSize.height)), Point(left + round(1.5*labelSize.width), top + baseLine), Scalar(255, 255, 255), FILLED);
+
+    // Find bounding rectangle of ball and draw in blue
+    rectangle(frame, Point(left, top - round(1.5*labelSize.height)), Point(left + round(1.5*labelSize.width), top + baseLine), Scalar(0, 255, 0), FILLED);
     putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,0),1);
+
+    return true;
 }
 
 // Remove the bounding boxes with low confidence using non-maxima suppression
-void postprocess(Mat& frame, const vector<Mat>& outs)
+Rect postprocess(Mat& frame, const vector<Mat>& outs)
 {
     vector<int> classIds;
     vector<float> confidences;
@@ -101,23 +120,23 @@ void postprocess(Mat& frame, const vector<Mat>& outs)
     {
         int idx = indices[i];
         Rect box = boxes[idx];
-        drawPred(classIds[idx], confidences[idx], box.x, box.y,
+        bool success = drawPred(classIds[idx], confidences[idx], box.x, box.y,
                  box.x + box.width, box.y + box.height, frame);
+
+        if (success)
+        {
+            return box;
+        }
     }
+
+    Rect empty(0, 0, 0, 0);
+    return empty;
 }
 
 // Detects the location of the ball and returns location
 Rect findBallLocation(Mat frame)
 {
     // Use detection to find the location of the ball
-    // Initialize the parameters
-    float objectnessThreshold = 0.5; // Objectness threshold
-    float confThreshold = 0.5; // Confidence threshold
-    float nmsThreshold = 0.4;  // Non-maximum suppression threshold
-    int inpWidth = 416;  // Width of network's input image
-    int inpHeight = 416; // Height of network's input image
-    vector<string> classes;
-
     // Load names of classes
     string classesFile = "coco.names";
     ifstream ifs(classesFile.c_str());
@@ -140,23 +159,11 @@ Rect findBallLocation(Mat frame)
     net.forward(outs, getOutputsNames(net));
 
     // Remove the bounding boxes with low confidence
-    postprocess(frame, outs);
-    // Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-    vector<double> layersTimes;
-    double freq = getTickFrequency() / 1000;
-    double t = net.getPerfProfile(layersTimes) / freq;
-    string label = format("Inference time for a frame : %.2f ms", t);
-    putText(frame, label, Point(0, 15), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255));
-
-    // Find bounding rectangle of ball and draw in blue
-
-
-    // Return the bounding rectangle of the ball (if not found, let user know)
-
+    return postprocess(frame, outs);
 }
 
 // Tracks the ball
-bool trackBall(Ptr<Tracker> tracker, Mat frame, Rect2d location, bool foundLastFrame)
+bool trackBall(Ptr<Tracker> tracker, Mat frame, Rect2d location)
 {
     // Use an OpenCV tracker to track the soccer ball location
     // Update the tracking result
@@ -189,9 +196,6 @@ bool trackBall(Ptr<Tracker> tracker, Mat frame, Rect2d location, bool foundLastF
 
 int main()
 {
-    // Train the YOLO detector
-
-
     // Create a KCF tracker
     Ptr<Tracker> tracker = TrackerKCF::create();
 
@@ -207,6 +211,11 @@ int main()
     // Read first frame
     Mat frame;
     bool ok = cap.read(frame);
+
+    if (!ok)
+    {
+        cout << "Unable to read video frame" << endl;
+    }
 
     // Initialize tracker
     Rect2d ballLocation = findBallLocation(frame);
@@ -234,7 +243,14 @@ int main()
             location = findBallLocation(frame);
         }
 
-        foundLastFrame = trackBall(tracker, frame, location, foundLastFrame);
+        if (!foundLastFrame && location.height != 0)
+        {
+            foundLastFrame = trackBall(tracker, frame, location);
+        }
+        else
+        {
+            putText(frame, "Ball Not Found in Frame", Point(100,20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 255),2);
+        }
 
         // Display the found ball location
         imshow("Window", frame);
